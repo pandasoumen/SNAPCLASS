e3273dd6b75a93f05c1e578c7b775894a3df76ae
pandasoumen <pandasoumen445@gmail.com>
2026-09-09 23:38:07 +0530

    streamlit  + homescreen

--- app.py
+++ app.py
@@ -1,19 +1,75 @@
 
+# import streamlit as st
+
+# from src.screens.home_screen import home_screen
+# from src.screens.student_screen import student_screen
+# from src.screens.teacher_screen import teacher_screen
+
+# def main():
+#     # st.header("Welcome to the Streamlit App!")
+#     # name = st.text_input("Enter your name:")
+
+
+#     # col1, col2 = st.columns(2)
+#     # with col1:
+#     #     if st.button('Display my name',type='primary', key='btn1'):
+#     #         print("Hello", name)
+#     # with col2:
+#     #     if st.button('Clear my name',type='primary', key='btn2'):
+#     #         name = ""
+#     # st.markdown("""
+
+#     # """, unsafe_allow_html=True)
+
+#     if 'login_type' not in st.session_state:
+#         st.session_state['login_type'] = None
+
+#     match st.session_state['login_type']:
+#         case 'teacher':
+#             teacher_screen()
+
+#         case 'student':
+#             student_screen()
+
+#         case None:
+#             home_screen()
+
+# main()
+
+
+
 import streamlit as st
 
+from src.screens.home_screen import home_screen
+from src.screens.teacher_screen import teacher_screen
+from src.screens.student_screen import student_screen
+
+from src.components.dialog_auto_enroll import auto_enroll_dialog
+
 def main():
-    st.header("Welcome to the Streamlit App!")
-    name = st.text_input("Enter your name:")
+    st.set_page_config(
+        page_title='SnapClass - Making Attendance faster using AI',
+        page_icon= "https://i.ibb.co/YTYGn5qV/logo.png"
+    )
+    if 'login_type' not in st.session_state:
+        st.session_state['login_type'] = None
+
+    match st.session_state['login_type']:
+        case 'teacher':
+            teacher_screen()
+
+        case 'student':
+            student_screen()
+        
+        case None:
+            home_screen()
 
 
-    col1, col2 = st.columns(2)
-    with col1:
-        if st.button('Display my name',type='primary', key='btn1'):
-            print("Hello", name)
-    with col2:
-        if st.button('Clear my name',type='primary', key='btn2'):
-            name = ""
-    st.markdown("""
-
-    """, unsafe_allow_html=True)
+    join_code = st.query_params.get('join-code')
+    if join_code:
+        if st.session_state.login_type != 'student':
+            st.session_state.login_type = 'student'
+            st.rerun()
+        if st.session_state.get('is_logged_in') and st.session_state.get('user_role') == 'student':
+            auto_enroll_dialog(join_code)
 main()
